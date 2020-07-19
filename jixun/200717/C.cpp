8344798045b7766a446960d04d72673991305709
rev-str <909363074@qq.com>
2020-07-19 11:29:54 +0800

    hello

--- jixun/200717/C.cpp
+++ jixun/200717/C.cpp
@@ -1,1 +1,15 @@
-  
+#include <bits/stdc++.h>
+using namespace std;
+const int maxn=6e5+10;
+struct ACautomation{
+    struct Node{
+    int fail,cnt,last,next[26];
+    }tree[maxn*4];
+    int tot;
+    void init(){
+        
+    }
+}
+int main(){
+    
+}
